409be3152bccb6459cf8550c42a7c2a449040ff6
unknown <sianasco@espe.edu.ec>
2022-11-29 06:36:28 -0500

    Code changes

---
+++ 06-Code/ApanadoMenu/src/ec/edu/espe/apanadoMenu/model/CreditPaymentServiceBank.java
@@ -0,0 +1,37 @@
+package ec.edu.espe.apanadoMenu.model;
+
+/**
+ *
+ * @author Añasco Silvia, DeltaTeam, DCCO-ESPE
+ */
+public class CreditPaymentServiceBank {
+    private int cardNumber;
+    private String bankName;
+    private String customerName;
+
+    public int getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(int cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+    
+    
+}
